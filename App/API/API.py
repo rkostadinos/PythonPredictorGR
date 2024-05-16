e0a66800868d5d88afbd0f08ecda7a1b584ea224
hydro <101656174+rkostadinos@users.noreply.github.com>
2024-05-16 20:56:29 +0300

    Update API.py
    comments in the API code

--- App/API/API.py
+++ App/API/API.py
@@ -1,9 +1,9 @@
 import requests
 import json
-
+# Ο σύνδεσμος του API και το token για να μπορέσουμε να χρησιμοποιήουμε τα δεδομένα του API
 uri = 'https://api.football-data.org/v4/matches'
 headers = { 'X-Auth-Token': 'f3512f1017274e95a9559ea0d9a2debf'}
-
+# βάζουμε σε μεταβλητή την απάντηση του API και χρησιμοποιήται λούπα για να εξάγουμε τα δεδομένα σε μορφή json
 response = requests.get(uri, headers=headers)
 for match in response.json()['matches']:
   print(match)
