f6b09e740df5765957daef940efd867507cfe900
az04-is <nz200026@gmail.com>
2024-09-09 14:00:10 +0300

    Final GUI

--- launch.bat
+++ launch.bat
@@ -4,7 +4,7 @@ python --version || winget install -e --id Python.Python.3.12 --scope machine
 
 pip install -r .\requirements.txt >nul 2>&1
 
-cd .\App\GUI
-.\GUI.py
+cd .\App\GUI || cd App\GUI
+.\GUI.py || GUI.py
 
 pause
