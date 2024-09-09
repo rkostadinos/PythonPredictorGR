5e6c1c4451087c80976537e52a495a18ddfa010d
az04-is <nz200026@gmail.com>
2024-09-09 16:54:58 +0300

    Final GUI

--- App/GUI/GUI.py
+++ App/GUI/GUI.py
@@ -9,8 +9,8 @@ import os
 import sys
 import json
 
-#inf2023055  Αχιλλέας Ζήσης
-#Βοήθεια για τον κώδικα πήρα από: https://www.youtube.com/watch?v=ibf5cx221hk
+# inf2023055	Αχιλλέας Ζήσης
+# Βοήθεια για τον κώδικα πήρα από: https://www.youtube.com/watch?v=ibf5cx221hk
 
 # Ορισμός του βασικού φακέλου για τη χρήση των άλλων αρχείων
 base_dir = os.path.dirname(os.path.abspath(__file__))
@@ -55,8 +55,6 @@ def refresh_matches(matches_window):
 def get_player_predictions(match):
     # Νέο παράθυρο για τον Παίκτη 1
     player1_pred_window = tk.Toplevel(root)
-    # player1_pred_window.title("Πρόβλεψη Παίκτη 1")
-    # player1_pred_window.geometry("300x200")
     player1_pred_window.withdraw()
 
     player1_score = simpledialog.askstring("Πρόβλεψη Παίκτη 1", f"Δώσε την πρόβλεψή σου για τον επιλεγμένο αγώνα:")
@@ -68,8 +66,6 @@ def get_player_predictions(match):
 
     # Νέο παράθυρο για τον Παίκτη 2
     player2_pred_window = tk.Toplevel(root)
-    # player2_pred_window.title("Πρόβλεψη Παίκτη 2")
-    # player2_pred_window.geometry("300x200")
     player2_pred_window.withdraw()
 
     player2_score = simpledialog.askstring("Πρόβλεψη Παίκτη 2", f"Δώσε την πρόβλεψή σου για τον επιλεγμένο αγώνα:")
@@ -111,28 +107,25 @@ def calculate_points(player1_score, player2_score, match_result):
     actual_away_score = match_result['awayTeam']
 
     if (player1_predicted_home == actual_home_score and player1_predicted_away == actual_away_score):
-        player1_points = 3  # Ο Παίκτης 1 πήρε σωστά το σκορ
-    elif (player1_predicted_home > player1_predicted_away and actual_home_score > actual_away_score) or (player1_predicted_home < player1_predicted_away and actual_home_score < actual_away_score):
-        player1_points = 2  # Ο Παίκτης 1 βρήκε τη νικήτρια ομάδα
+        player1_points += 3  # Ο Παίκτης 1 πήρε σωστά το σκορ
+    elif (player1_predicted_home > player1_predicted_away and actual_home_score > actual_away_score) or (player1_predicted_home < player1_predicted_away and actual_home_score < actual_away_score) or (player1_predicted_home == player1_predicted_away and actual_home_score == actual_away_score):
+        player1_points += 2  # Ο Παίκτης 1 βρήκε τη νικήτρια ομάδα/την ισοπαλία
 
     if (player2_predicted_home == actual_home_score and player2_predicted_away == actual_away_score):
-        player2_points = 3  # Ο Παίκτης 2 πήρε σωστά το σκορ
-    elif (player2_predicted_home > player2_predicted_away and actual_home_score > actual_away_score) or (player2_predicted_home < player2_predicted_away and actual_home_score < actual_away_score):
-        player2_points = 2  # Ο Παίκτης 2 βρήκε τη νικήτρια ομάδα
+        player2_points += 3  # Ο Παίκτης 2 πήρε σωστά το σκορ
+    elif (player2_predicted_home > player2_predicted_away and actual_home_score > actual_away_score) or (player2_predicted_home < player2_predicted_away and actual_home_score < actual_away_score) or (player2_predicted_home == player2_predicted_away and actual_home_score == actual_away_score):
+        player2_points += 2  # Ο Παίκτης 2 βρήκε τη νικήτρια ομάδα/την ισοπαλία
 
     return player1_points, player2_points
 
 # Συνάρτηση που εμφανίζει τον νικητή με βάση τους πόντους
 def show_winner(player1_points, player2_points):
     if player1_points > player2_points:
-        messagebox.showinfo("Νικητής", f"Ο Παίκτης 1 κερδίζει με {player1_points} πόντους!")
-        messagebox.showinfo("",f"Ο Παίκτης 2 συγκέντρωσε {player2_points} πόντους!")
+        messagebox.showinfo("Νικητής", f"Ο παίκτης 1 κερδίζει με {player1_points} πόντους, ενώ ο παίκτης 2 συγκέντρωσε {player2_points} πόντους!")
     elif player2_points > player1_points:
-        messagebox.showinfo("Νικητής", f"Ο Παίκτης 2 κερδίζει με {player2_points} πόντους!")
-        messagebox.showinfo("",f"Ο Παίκτης 1 συγκέντρωσε {player1_points} πόντους!")
+        messagebox.showinfo("Νικητής", f"Ο παίκτης 2 κερδίζει με {player2_points} πόντους, ενώ ο παίκτης 1 συγκέντρωσε {player1_points} πόντους!")
     else:
-        messagebox.showinfo("Ισοπαλία", "Η πρόβλεψη είναι ισοπαλία!")
-        messagebox.showinfo("",f"Οι Παίκτες συγκέντρωσαν {player2_points} πόντους!")
+        messagebox.showinfo("Ισοπαλία", "Η πρόβλεψη είναι ισοπαλία, οι παίκτες συγκέντρωσαν {player1_points} πόντους!")
 
 # Εύρεση αποτελεσμάτων αγώνα
 def check_results():
