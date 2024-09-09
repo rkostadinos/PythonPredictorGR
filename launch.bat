f315a4268d41368297ba2e1691a35f2c4b64562d
az04-is <nz200026@gmail.com>
2024-09-09 12:55:52 +0300

    Final GUI

--- launch.bat
+++ launch.bat
@@ -2,7 +2,7 @@
 
 python --version || (winget install -e --id Python.Python.3.12 --scope machine || pacman -Sy python || apt-get install python || dnf install python || yum install python)
 
-(pip install -r ./requirements.txt || pip install -r .\requirements.txt) > nul 2>&1
+(pip install -r ./requirements.txt || pip install -r .\requirements.txt)
 
 cd ./App/GUI || cd .\App\GUI
 ./GUI.py || .\GUI.py
